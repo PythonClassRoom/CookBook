e18d5dafe8d76998204686e656d8c5a43acc9064
Adrian Prendas <a6r1an@hotmail.com>
2018-04-27 19:46:45 -0600

    class with init method

--- db_postgres.py
+++ db_postgres.py
@@ -4,7 +4,7 @@ CONN_INFO = {
     'host': '127.0.0.1',
     'port': 5432,
     'user': 'postgres',
-    'pass': 'root',
+    'pass': 'root'
 }
 
 CONN_STR = 'host={host} port={port} user={user} password={pass}'.format(**CONN_INFO)
